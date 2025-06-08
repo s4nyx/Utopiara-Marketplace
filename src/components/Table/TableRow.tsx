771c65777db2450843f25d40aa9d99c9f62a7f44
slark39 <sanyscott4139@gmail.com>
2025-06-08 10:10:41 -0700

    chore:table row hover detail page

--- src/components/Table/TableRow.tsx
+++ src/components/Table/TableRow.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import Image from "next/image";
+import React, { useState } from "react";
 
 interface CollectionProps {
   index: number;
@@ -21,10 +22,69 @@ export default function TableRow({
   unique_owners,
   items_listed,
 }: CollectionProps) {
+  const [isDetailShowing, setIsDetailShowing] = useState(false);
+
   return (
-    <div className="mt-4 flex w-full flex-row items-center justify-between text-xl text-white">
+    <div className="mt-4 flex relative w-full flex-row items-center justify-between text-xl text-white">
+      {isDetailShowing && (
+        <div className="absolute -bottom-[200px] w-[450px] h-[200px] rounded-lg p-2 flex flex-col items-start justify-between text-white bg-third z-10">
+          <div className="text-xl">{collection}</div>
+          <div className="flex flex-row w-full justify-between border-b-[1px] p-1 border-secondary">
+            <Image
+              src={"/images/1.jpg"}
+              alt="image"
+              width={200}
+              height={100}
+              className="h-[100px] w-[135px]"
+            />
+            <Image
+              src={"/images/1.jpg"}
+              alt="image"
+              width={200}
+              height={100}
+              className="h-[100px] w-[135px]"
+            />
+            <Image
+              src={"/images/1.jpg"}
+              alt="image"
+              width={200}
+              height={100}
+              className="h-[100px] w-[135px]"
+            />
+          </div>
+          <div className="flex flex-row w-full gap-2 justify-between items-start">
+            <div className="flex flex-col items-start gap-2">
+              <h1 className="text-sm">FLOOR PRICE</h1>
+              <p className="text-sm">0.003 ETH</p>
+            </div>
+            <div className="flex flex-col items-start gap-2">
+              <h1 className="text-sm">FLOOR PRICE</h1>
+              <p className="text-sm">0.003 ETH</p>
+            </div>
+            <div className="flex flex-col items-start gap-2">
+              <h1 className="text-sm">FLOOR PRICE</h1>
+              <p className="text-sm">0.003 ETH</p>
+            </div>
+            <div className="flex flex-col items-start gap-2">
+              <h1 className="text-sm">FLOOR PRICE</h1>
+              <p className="text-sm">0.003 ETH</p>
+            </div>
+          </div>
+        </div>
+      )}
+
       <div className="basis-[5%]">{index}</div>
-      <div className="basis-[35%]">{collection}</div>
+      <div
+        className="basis-[35%] cursor-pointer"
+        onMouseEnter={() => {
+          setIsDetailShowing(true);
+        }}
+        onMouseLeave={() => {
+          setIsDetailShowing(false);
+        }}
+      >
+        {collection}
+      </div>
       <div className="basis-[10%]">{volume}</div>
       <div className="basis-[10%]">{change}</div>
       <div className="basis-[10%]">{floor_price}</div>
